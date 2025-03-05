b18fc4a68761223a09d6b3760d13c1a5fe475829
yemiwebby <yemiwebby@gmail.com>
2025-03-05 10:23:11 +0000

    update database connection

--- app.js
+++ app.js
@@ -46,9 +46,10 @@ app.use(function (err, req, res) {
 })
 
 // Connect to db
+// mongoose
+//     .connect(process.env.DATABASE_URI)
 mongoose
-    .connect(process.env.DATABASE_URI)
-    // mongoose.connect(process.env.DB_CONNECTION)
+    .connect(process.env.DB_CONNECTION)
     .then(() => console.log('DB connected!'))
     .catch((err) => console.log(err))
 
